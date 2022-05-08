7cc506f82cb030ec6f278041d34f100647d3089b
Emmy Veselinov <emmyveselinov@gmail.com>
2022-05-07 21:08:41 -0400

    added function for task D

--- funcs.cpp
+++ funcs.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cctype>
 #include "funcs.h"
 
 //task A
@@ -46,5 +47,27 @@ int sumArray(int *arr, int size){
         //dereferences the size - 1th index (aka starts with last index and works its way down to 0) then makes a recursive call with a size lower to add the value of the next element
         //wouldn't work if we did *(arr + size) b/c that would dereference the value at index size, BUT the last index of an array is size - 1
     }
+
+}
+
+//task D
+bool isAlphanumeric(std::string s){
+    //returns true if ALL characters are letters and digits, otherwise return false
+    //substr(pos,len)
+    
+    if (s.size() == 0){ //base case
+        return true;
+    }
+
+    else if (!isalpha(s[s.size() - 1]) && !isdigit(s[s.size() - 1])){
+        return false;
+    }
+
+    else {
+        return isAlphanumeric(s.substr(0, s.size() - 1)); //decreases the size of the string by 1 (pos=0, len=lowers by 1)
+        //the above else if statement would check if the last element of that newly constructed string is not alphanumeric
+        //if it is alphanumeric, the string size is decreased by 1 and it do it all again till the size is 0 --> true
+    }
+    
 
 }
