e8f70976d959e881223fd040058fad2c3c57c055
Emmy Veselinov <emmyveselinov@gmail.com>
2022-05-07 21:08:55 -0400

    added function declaration for task D

--- funcs.h
+++ funcs.h
@@ -1,10 +1,13 @@
 #pragma once
-#include "funcs.h"
 #include <iostream>
+#include <cctype>
+#include "funcs.h"
 
 std::string printRange(int left, int right);
 
 int sumRange(int left, int right);
 
 int sumArray(int *arr, int size);
+
+bool isAlphanumeric(std::string s);
 
